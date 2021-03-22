b219c4af4fd9f20021fa8cb5fb4f0e08c9dda1f7
Alexia <alexheg44@gmail.com>
2021-03-21 21:52:14 -0400

    commit tree e023467c84498210a7a66b24e686efefe2a11ccf

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,30 @@
 
+class Artist
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+
+        @@all.push(self)
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_song(name, genre)
+        Song.new(name, self, genre)
+    end
+
+    def songs
+        Song.all.select {|songs| songs.artist == self}
+    end
+
+    def genres
+        Song.all.select {|genres| 
+            genres.artist == self}.map {|genre_instances| 
+                genre_instances.genre}
+    end
+end
